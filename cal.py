e2bdfc98a5898191371e9cd6032a8996ad3d88e7
hyunj24 <hyunjug7@naver.com>
2024-10-01 22:35:56 +0900

    added multi and divide

--- cal.py
+++ cal.py
@@ -1,10 +1,14 @@
 num1 = int(input("input integer1: "))
 num2 = int(input("input integer2: "))
-op = input("input operator(+ / -): ")
+op = input("input operator(+ / - / * / /): ")
 
 if op == '+':
     print(num1, "+", num2, "=", num1 + num2)
 elif (op == '-'):
     print(num1, "-", num2, "=", num1-num2)
+elif (op == '*'):
+    print(num1, "*", num2, "=", num1*num2)
+elif (op == '/'):
+    print(num1, "/", num2, "=", num1/num2)
 else:
     print("error")
