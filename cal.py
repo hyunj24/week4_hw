3accc2e04e255f2dd6dd24ee61f983f2119f667b
hyunj24 <hyunjug7@naver.com>
2024-10-01 22:40:30 +0900

    change or

--- cal.py
+++ cal.py
@@ -1,6 +1,6 @@
 num1 = int(input("input integer1: "))
 num2 = int(input("input integer2: "))
-op = input("input operator(+ / - / * / /): ")
+op = input("input operator(+ or - or * or /): ")
 
 if op == '+':
     print(num1, "+", num2, "=", num1 + num2)
