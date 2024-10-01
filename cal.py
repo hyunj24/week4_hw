5b099e9d52c26a50f3ba4844107e4e1a4b2700b5
hyunj24 <hyunjug7@naver.com>
2024-10-01 22:41:48 +0900

    change comma

--- cal.py
+++ cal.py
@@ -1,6 +1,6 @@
 num1 = int(input("input integer1: "))
 num2 = int(input("input integer2: "))
-op = input("input operator(+ / - / * / /): ")
+op = input("input operator(+ , - , * , /): ")
 
 if op == '+':
     print(num1, "+", num2, "=", num1 + num2)
